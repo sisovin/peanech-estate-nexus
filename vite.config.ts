95b71feb146f47329e31955bf6c433fd7e2bc37f
Your Name <you@example.com>
2025-07-21 19:46:16 +0700

    Update modified:   package-lock.json vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowHost: true,
   },
   plugins: [
     react(),
